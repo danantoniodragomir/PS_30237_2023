f931b56e30c0f6543a60753075a5c0fef1650b70
danantoniodragomir <danantoniodragomir@gmail.com>
2023-03-27 03:52:28 +0300

    first commit

---
+++ src/test/java/com/example/watchmarket/WatchMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
